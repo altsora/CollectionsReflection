1f5746ef805c749e9ac83d7e4da19e523c05e903
altsora <vergun_a.s@mail.ru>
2020-11-22 15:27:36 +0300

    fix ArrayList

--- src/main/java/main/ArrayListReflection.java
+++ src/main/java/main/ArrayListReflection.java
@@ -5,32 +5,19 @@ import lombok.extern.log4j.Log4j2;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Log4j2
 public class ArrayListReflection {
+    private static List<String> list = new ArrayList<>();
+
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>();
         showInside(list);
-        log.info("+ 1");
-        list.add("a");
-        showInside(list);
-        log.info("+ 2");
-        list.add("b");
-        list.add("c");
-        showInside(list);
-        log.info("+ 7");
-        list.add("d");
-        list.add("e");
-        list.add("f");
-        list.add("g");
-        list.add("h");
-        list.add("i");
-        list.add("j");
-        showInside(list);
-        log.info("+ 1");
-        list.add("k");
-        showInside(list);
+        add("a");
+        add("b", "c");
+        add("d", "e", "f", "g", "h", "i", "j");
+        add("k");
     }
 
     @SneakyThrows
@@ -52,5 +39,11 @@ public class ArrayListReflection {
             log.info("\t{}.\t{}", i, objects[i]);
         }
         System.out.println();
+    }
+
+    private static void add(String... s) {
+        log.info("+ {}", s.length);
+        list.addAll(Arrays.asList(s));
+        showInside(list);
     }
 }
